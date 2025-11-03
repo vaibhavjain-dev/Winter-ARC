09173e0a2ac420f61633aa6dd484556055f9d6ae
vaibhavjain-dev <vaibhavjain_dev@outlook.com>
2025-11-03 14:56:45 +0530

    feat: add object example demonstrating key-value pairs and method usage

--- Winter-ARC'25/Web-Dev/Javascript/Code_Help (Love Babber)/5-Objects && Arrays.js
+++ Winter-ARC'25/Web-Dev/Javascript/Code_Help (Love Babber)/5-Objects && Arrays.js
@@ -1,2 +1,20 @@
-console.log("\n--- Objects && Arrays in JS ---\n")
+console.log("\n--- Objects in JS ---\n");
 
+console.log("Object : collection of data in key : value pairs");
+
+let obj = {
+    firstName: "Vaibhav",
+    lastName: "Jain",
+    age: 18,
+    role: "Web3 Developer",
+    Salary: 10000000,
+    greet: function () {
+        console.log("That's my profile! Hire me.");
+    }
+};
+
+console.table(obj);
+obj.greet();
+
+let obj2 = obj;
+console.log("Copied object (obj2):", obj2);
