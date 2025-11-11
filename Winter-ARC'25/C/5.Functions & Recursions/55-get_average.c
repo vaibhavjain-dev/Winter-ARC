7ba5f4ec1c735f6991886f64ad7b4946139f9ae3
vaibhavjain-dev <vaibhavjain_dev@outlook.com>
2025-11-12 01:43:42 +0530

    feat: add function program to calculate average of five numbers in C

--- Winter-ARC'25/C/5.Functions & Recursions/55-get_average.c
+++ Winter-ARC'25/C/5.Functions & Recursions/55-get_average.c
@@ -1,21 +1,29 @@
+// -------------------------------------------------------------
+// 💠 Program Title: Calculate Average of Five Numbers using Function
+// 💻 Description: Demonstrates how to pass multiple float arguments
+//                 to a user-defined function and return their average.
+// -------------------------------------------------------------
+
 #include <stdio.h>
 
+// Function definition
 float get_Average(float a, float b, float c, float d, float e)
 {
-    float Avg = (a + b + c + d + e) / 5.0;
-    return Avg;
+    float avg = (a + b + c + d + e) / 5.0;
+    return avg;
 }
 
 int main()
 {
     float a, b, c, d, e;
 
-    printf("---Find Avg of 5 nums using function---\n");
+    printf("--- Average Calculation using Function ---\n");
     printf("Enter 5 numbers: ");
     scanf("%f %f %f %f %f", &a, &b, &c, &d, &e);
 
-    float Average = get_Average(a, b, c, d, e);
+    float average = get_Average(a, b, c, d, e);
 
-    printf("\nAverage of 5 numbers is: %.2f\n", Average);
+    printf("\nThe average of the 5 numbers is: %.2f\n", average);
+
     return 0;
 }
