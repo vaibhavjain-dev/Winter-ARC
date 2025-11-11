8143248514b2753833b37d38e9be0f43bbbb1bbc
vaibhavjain-dev <vaibhavjain_dev@outlook.com>
2025-11-12 01:43:18 +0530

    feat: add call by value demonstration program showing variable behavior inside and outside a function

--- Winter-ARC'25/C/5.Functions & Recursions/54-Increment.c
+++ Winter-ARC'25/C/5.Functions & Recursions/54-Increment.c
@@ -1,22 +1,31 @@
+// -------------------------------------------------------------
+// 💠 Program Title: Demonstration of Call by Value in C
+// 💻 Description: Shows how variables are passed by value to a function.
+//                 The original value in main() remains unchanged even
+//                 after modification inside the function.
+// -------------------------------------------------------------
+
 #include <stdio.h>
 
+// Function definition
 void Increment(int a)
 {
-    printf("In Function Before The value of Num is %d\n", a);
+    printf("Inside Function: Before Increment, Num = %d\n", a);
     a++;
-    printf("In Function After Increment : The value of Num is %d\n", a);
+    printf("Inside Function: After Increment, Num = %d\n", a);
 }
 
 int main()
 {
     int num;
 
-    printf("\n---Welcome to showing call by value---\n");
-    printf("\nPlease enter the number: ");
+    printf("--- Welcome to Call by Value Demonstration ---\n");
+    printf("Enter a number: ");
     scanf("%d", &num);
 
-    printf("The value of Num is %d\n", num);
+    printf("\nBefore Function Call: Num = %d\n", num);
     Increment(num);
-    printf("After Increment : The value of Num is %d\n", num);
+    printf("After Function Call: Num = %d\n", num);
+
     return 0;
 }
